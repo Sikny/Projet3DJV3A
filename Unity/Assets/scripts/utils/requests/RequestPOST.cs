3277128780851b48cd825b9773549b5ea81051bf
brete <bretellealan@hotmail.fr>
2020-01-12 22:56:49 +0100

    modelisations + préparation WS requesting

---
+++ Unity/Assets/scripts/utils/requests/RequestPOST.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    public static string Post(string address, Dictionary<string, string>  param)
+    {
+        //TODO 
+        return "";
+    }
+}
