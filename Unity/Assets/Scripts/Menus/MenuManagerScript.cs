24a0fde607b0cf8784f711d9c29e49eaf7210607
sikny <pamartqu@gmail.com>
2020-01-13 19:42:35 +0100

    Connect client with email+pwd, prefab organization for UI

---
+++ Unity/Assets/Scripts/Menus/MenuManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Menus {
+    public class MenuManagerScript : MonoBehaviour {
+        public GameObject optionsPanel;
+        public GameObject connectionPanel;
+        public GameObject registerPanel;
+        public void OnConnectButtonPressed() {
+            connectionPanel.SetActive(true);
+        }
+
+        public void OnOptionsButtonPressed() {
+            optionsPanel.SetActive(true);
+        }
+
+        public void OnOptionsBackButtonPressed() {
+            optionsPanel.SetActive(false);
+        }
+
+        public void OnConnectConfirmPressed() {
+            //StartCoroutine(ConnectModule.Instance.ConnectUser());
+            //  TODO CONNECT, LOADING, CONFIRM WINDOW
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/WebClient/ConnectModule.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace WebClient {
+    public class ConnectModule {
+        private static ConnectModule _instance;  
+        public static ConnectModule Instance {
+            get {
+                if(_instance == null) _instance = new ConnectModule();
+                return _instance;
+            }
+        }
+
+        /**
+         * Connects user with given login & password
+         */
+        public IEnumerator ConnectUser(string login, string password) {
+            List<IMultipartFormSection> formData = new List<IMultipartFormSection> {
+                new MultipartFormDataSection("mail", login),
+                new MultipartFormDataSection("psw", password)
+            };
+            
+            UnityWebRequest www = UnityWebRequest.Post("http://piwelengine.eu/sauron/WS/LOGIN.php", formData);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError) {
+                Debug.Log(www.error);
+            }
+            else {
+                Debug.Log("Connect success");
+                string result = www.downloadHandler.text;
+                Debug.Log("Received: " + result);
+            }
+        }
+
+        /**
+         * Valids given token
+         */
+        public IEnumerator ValidToken(string token) {
+            List<IMultipartFormSection> formData = new List<IMultipartFormSection> {
+                new MultipartFormDataSection("token", token),
+            };
+            
+            UnityWebRequest www = UnityWebRequest.Post("http://piwelengine.eu/sauron/WS/VALID_TOKEN.php", formData);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError) {
+                Debug.Log(www.error);
+            }
+            else {
+                Debug.Log("Connect success");
+                string result = www.downloadHandler.text;
+                Debug.Log("Received: " + result);
+            }
+        }
+    }
+}
