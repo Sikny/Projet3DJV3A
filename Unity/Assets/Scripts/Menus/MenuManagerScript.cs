7b9f76ee511f411c4b6e389f85105c0fe10aca74
sikny <pamartqu@gmail.com>
2020-01-15 18:26:52 +0100

    UI update

--- Unity/Assets/Scripts/Menus/MenuManagerScript.cs
+++ Unity/Assets/Scripts/Menus/MenuManagerScript.cs
@@ -1,25 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
+using WebClient;
 
 namespace Menus {
     public class MenuManagerScript : MonoBehaviour {
         public GameObject optionsPanel;
-        public GameObject connectionPanel;
+        public ConnectForm connectionPanel;
         public GameObject registerPanel;
-        public void OnConnectButtonPressed() {
-            connectionPanel.SetActive(true);
-        }
-
-        public void OnOptionsButtonPressed() {
-            optionsPanel.SetActive(true);
-        }
-
-        public void OnOptionsBackButtonPressed() {
-            optionsPanel.SetActive(false);
-        }
-
+        
         public void OnConnectConfirmPressed() {
+            connectionPanel.confirmButton.enabled = false;
+            StartCoroutine(ConnectModule.Instance.ConnectUser(connectionPanel.mail.text, connectionPanel.password.text, ProcessConnectionResult));
             //StartCoroutine(ConnectModule.Instance.ConnectUser());
             //  TODO CONNECT, LOADING, CONFIRM WINDOW
+        }
+
+        private void ProcessConnectionResult(UnityWebRequest www) {
+            if (www.isNetworkError || www.isHttpError) {
+                Debug.Log(www.error);
+            }
+            else {
+                Debug.Log("Connect success");
+                string result = www.downloadHandler.text;
+                Debug.Log("Received: " + result);
+            }
         }
     }
 }
--- Unity/Assets/Scripts/WebClient/ConnectModule.cs
+++ Unity/Assets/Scripts/WebClient/ConnectModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -16,7 +17,7 @@ namespace WebClient {
         /**
          * Connects user with given login & password
          */
-        public IEnumerator ConnectUser(string login, string password) {
+        public IEnumerator ConnectUser(string login, string password, Action<UnityWebRequest> callback) {
             List<IMultipartFormSection> formData = new List<IMultipartFormSection> {
                 new MultipartFormDataSection("mail", login),
                 new MultipartFormDataSection("psw", password)
@@ -24,19 +25,11 @@ namespace WebClient {
             
             UnityWebRequest www = UnityWebRequest.Post("http://piwelengine.eu/sauron/WS/LOGIN.php", formData);
             yield return www.SendWebRequest();
-
-            if (www.isNetworkError || www.isHttpError) {
-                Debug.Log(www.error);
-            }
-            else {
-                Debug.Log("Connect success");
-                string result = www.downloadHandler.text;
-                Debug.Log("Received: " + result);
-            }
+            callback(www);
         }
 
         /**
-         * Valids given token
+         * Validates given token
          */
         public IEnumerator ValidToken(string token) {
             List<IMultipartFormSection> formData = new List<IMultipartFormSection> {
@@ -54,6 +47,32 @@ namespace WebClient {
                 string result = www.downloadHandler.text;
                 Debug.Log("Received: " + result);
             }
+        }
+
+        /**
+         * Registers new user with given data
+         */
+        public IEnumerator RegisterUser(string firstname, string lastname, string mail, string password, string confPassword) {
+            // TODO VALUES CHECK
+            List<IMultipartFormSection> formData = new List<IMultipartFormSection> {
+                new MultipartFormDataSection("firstname", firstname),
+                new MultipartFormDataSection("lastname", lastname),
+                new MultipartFormDataSection("mail", mail),
+                new MultipartFormDataSection("psw", password)
+            };
+
+            UnityWebRequest www = UnityWebRequest.Post("http://piwelengine.eu/sauron/WS/INSCRIPTION.php", formData);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError) {
+                Debug.Log(www.error);
+            }
+            else {
+                Debug.Log("Connect success");
+                string result = www.downloadHandler.text;
+                Debug.Log("Received: " + result);
+            }
+            //{firstname:"xxx",lastname:"xxx",mail:"aaa@bbb.fr","psw":1234,psw2:"1234"
         }
     }
 }
