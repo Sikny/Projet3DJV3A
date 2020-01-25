7d360669693cc8b66a04ef3c5ab56525f219350e
sikny <pamartqu@gmail.com>
2020-01-25 22:45:13 +0100

    Language editor, needs styling

---
+++ Unity/Assets/Editor/LanguageDictionaryEditPopup.cs
@@ -0,0 +1,31 @@
+﻿using Language;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    public class LanguageDictionaryEditPopup : EditorWindow {
+        private static string _value, _oldValue;
+        public static void EditKey(string value) {
+            LanguageDictionaryEditPopup window = CreateInstance<LanguageDictionaryEditPopup>();
+            _oldValue = value;
+            _value = value;
+            Rect r = window.position;
+            Vector2 position = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+            r.x = position.x;
+            r.y = position.y;
+            r.width = 200f;
+            r.height = 50f;
+            window.position = r;
+            window.ShowPopup();
+        }
+        
+        void OnGUI()
+        {
+            _value = GUILayout.TextField(_value);
+            if (GUILayout.Button("Ok")) {
+                LanguageDictionary.Instance.ModifyKey(_oldValue, _value);
+                Close();
+            }
+        }
+    }
+}
