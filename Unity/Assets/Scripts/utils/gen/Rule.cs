db2ae4da4dbf97f108c2820a69b18a02f37d7fe6
brete <bretellealan@hotmail.fr>
2020-01-27 22:01:23 +0100

    First tests with Serializable

--- Unity/Assets/Scripts/utils/gen/Rule.cs
+++ Unity/Assets/Scripts/utils/gen/Rule.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
+[Serializable]
 public class Rule
 {
     const int MAX_BUDGET = 2000;
@@ -15,9 +18,10 @@ public class Rule
     int maxBudget;
     int globalDifficulty;
 
+    //TODO : Serialize Vector2 into a new class (heritage?)
     Dictionary<Vector2, float> mapModifierHeightmap;
     Dictionary<Vector2, int> localSpawnDifficulty; //SPEC : to avoid the gen into the castle
-    Dictionary<Vector3, int> mapCastlePiecesPlacement; 
+    Dictionary<Vector3, int> mapCastlePiecesPlacement; // TODO : make castle class (including pos, rotation, scaling, id...)
 
     //TODO
     public Rule()
@@ -46,20 +50,48 @@ public class Rule
         this.globalDifficulty = 2;
 
         // Def authorized spawn
-        
-        for(float i = 0f; i <= Mathf.PI * 2; i += 1/(Mathf.PI * ENNEMY_SPAWN_RADIUS))
+        this.localSpawnDifficulty = new Dictionary<Vector2, int>();
+        for(float i = 0f; i <= Mathf.PI * 2; i += Mathf.PI / ( ENNEMY_SPAWN_RADIUS))
         {
             int xCoor = (int)(ENNEMY_SPAWN_RADIUS * Mathf.Cos(i));
             int zCoor = (int)(ENNEMY_SPAWN_RADIUS * Mathf.Sin(i));
 
             Vector2 posSpawner = new Vector2(xCoor, zCoor);
 
-            if(genRand.Next(0,5) == 0)
+            if(genRand.Next(0,20) == 0)
             {
                 // 5 levels of localdifficulty (0 = no spawn; 5 = max spawn)
-                this.localSpawnDifficulty.Add(posSpawner, genRand.Next(1, 5));
+                int levelOfDifficulty = genRand.Next(1, 5);
+                try
+                {
+                    this.localSpawnDifficulty.Add(posSpawner, levelOfDifficulty);
+                }
+                catch (ArgumentException ex)
+                {
+
+                }
+                Debug.Log("Pos " + posSpawner.ToString() + " : " + levelOfDifficulty);
             }
 
+        }
+    }
+
+    [Obsolete]
+    public static void saveLevel(string file, Rule r)
+    {
+        using (Stream stream = File.Open("levels/"+file+".lvl", FileMode.Create))
+        {
+            var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            binaryFormatter.Serialize(stream, r);
+        }
+    }
+    public static Rule readLevel(string file)
+    {
+        using (Stream stream = File.Open("levels/" + file + ".lvl", FileMode.Open))
+        {
+            var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            Rule r = (Rule)binaryFormatter.Deserialize(stream);
+            return r;
         }
     }
 }
