e4cab038dd414941ba0b886a8ed2b220d4c8f181
brete <bretellealan@hotmail.fr>
2020-01-28 22:39:52 +0100

    saveload rule basics

---
+++ LEVEL EDITOR/Assets/scripts/rule/RuleIOFile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleIOFile : MonoBehaviour
+{
+    static private Rule ruleLevel = null;
+    void Start()
+    {
+        ruleLevel = new Rule();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if(ruleLevel != null)
+            {
+                Rule.saveLevel("test",ruleLevel);
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.L))
+        {
+            if(ruleLevel == null)
+            {
+                ruleLevel = Rule.readLevel("test");
+            }
+        }
+    }
+}
