cfbd0567a6b5e7c787ed25c10965d24d1e12c83f
Neffros <46776595+Neffros@users.noreply.github.com>
2020-03-25 23:14:01 +0100

    implementing shopUI

---
+++ Unity/Assets/Scripts/UI/ShopManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class ShopManager : MonoBehaviour
+{
+    public TextMeshProUGUI goldText;
+    private int gold = 5;
+
+    public GameObject unitPanel;
+    public GameObject itemsPanel;
+    public GameObject equipmentsPanel;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gold = 10;
+        goldText.SetText(gold.ToString() + "g");
+    }
+    
+    
+    public void ShowUnitsPanel()
+    {
+        itemsPanel.SetActive(false);
+        equipmentsPanel.SetActive(false);
+        unitPanel.SetActive(true);
+    }
+    
+    public void ShowItemsPanel()
+    {
+        equipmentsPanel.SetActive(false);
+        unitPanel.SetActive(false);
+        itemsPanel.SetActive(true);
+    }
+
+    public void ShowEquipmentsPanel()
+    {
+        itemsPanel.SetActive(false);
+        unitPanel.SetActive(false);
+        equipmentsPanel.SetActive(true);
+    }
+    
+
+    
+
+}
