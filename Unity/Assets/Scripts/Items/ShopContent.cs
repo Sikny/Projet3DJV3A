976d70bccb7b925a56f34700e7a454b42bdfa0f8
Neffros <46776595+Neffros@users.noreply.github.com>
2020-03-31 21:16:56 +0200

    non functional implementation of shop

---
+++ Unity/Assets/Scripts/Items/ShopContent.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopContent : MonoBehaviour
+{
+    /*public List<Item> itemsConsummable = new List<Item>();
+    public List<Item> itemsEquipment = new List<Item>();
+    public List<Item> itemsUnits = new List<Item>();*/
+
+    public List<ItemSlot> itemSlotsConsumable = new List<ItemSlot>();
+
+    public List<Item> allItems;
+    
+    #region Singleton
+    
+    public static ShopContent instance;
+    
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Several instances");
+            return;
+        }
+        instance = this;
+    }
+
+
+    #endregion
+
+
+    public void ClearAllItems()
+    {
+        itemSlotsConsumable.Clear();
+        
+    }
+
+    public void FillItemSlot(Item newItem)
+    {
+        int nextIndex = itemSlotsConsumable.Count + 1;
+        itemSlotsConsumable[nextIndex].AddItem(newItem);
+        
+        if(itemSlotsConsumable[nextIndex].onItemChangedCallback != null)
+            itemSlotsConsumable[nextIndex].onItemChangedCallback.Invoke();
+    }
+
+/*
+    public void AddConsummable(Item item)
+    {
+        itemsConsummable.Add(item);
+    }
+
+    public void RemoveConsummable(Item item)
+    {
+        itemsConsummable.Remove(item);
+    }
+    
+    public void AddEquipment(Item item)
+    {
+        itemsEquipment.Add(item);
+    }
+
+    public void RemoveEquipment(Item item)
+    {
+        itemsEquipment.Remove(item);
+    }
+    public void AddUnit(Item item)
+    {
+        itemsUnits.Add(item);
+    }
+
+    public void RemoveUnit(Item item)
+    {
+        itemsUnits.Remove(item);
+    }*/
+    
+}
--- Unity/Assets/Scripts/UI/ShopManager.cs
+++ Unity/Assets/Scripts/UI/ShopManager.cs
@@ -11,13 +11,27 @@ public class ShopManager : MonoBehaviour
     public GameObject unitPanel;
     public GameObject itemsPanel;
     public GameObject equipmentsPanel;
+    public GameObject ShopPanel;
+
+    public GameObject prefab; 
     
+    public delegate void onUpdateUI(); 
+    public onUpdateUI onUpdateUICallback;
+
+    private ShopContent _shopContent;
     
     // Start is called before the first frame update
     void Start()
     {
+        _shopContent = ShopContent.instance;
         gold = 10;
         goldText.SetText(gold.ToString() + "g");
+        
+        GameObject currentSlot = prefab;
+        TextMeshProUGUI[] test = currentSlot.GetComponentsInChildren<TextMeshProUGUI>();
+        test[0].SetText("item name");
+        test[1].SetText("price");
+
     }
     
     
@@ -40,6 +54,28 @@ public class ShopManager : MonoBehaviour
         itemsPanel.SetActive(false);
         unitPanel.SetActive(false);
         equipmentsPanel.SetActive(true);
+    }
+
+    public void UpdateUI()
+    {
+        for (int i = 0; i < _shopContent.itemSlotsConsumable.Count - 1; i++)
+        {
+            
+            GameObject currentSlot = prefab;
+            TextMeshProUGUI[] texts = currentSlot.GetComponentsInChildren<TextMeshProUGUI>();
+            Image icon = currentSlot.GetComponentInChildren<Image>(); 
+            
+            texts[0].SetText(_shopContent.itemSlotsConsumable[i].item.name);
+            texts[1].SetText(_shopContent.itemSlotsConsumable[i].item.price.ToString());
+            icon.sprite = _shopContent.itemSlotsConsumable[i].item.icon;
+            
+            Instantiate(currentSlot);
+        }
+    }
+    
+    public void Fight()
+    {
+        ShopPanel.SetActive(false);
     }
     
 
