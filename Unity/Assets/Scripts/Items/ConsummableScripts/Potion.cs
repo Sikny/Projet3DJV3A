5cd4663a3b4af402eda8bdcfbb7055e0fa21fe43
Neffros <46776595+Neffros@users.noreply.github.com>
2020-04-05 00:49:55 +0200

    added consummable and equipment example

---
+++ Unity/Assets/Scripts/Items/ConsummableScripts/Potion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * sers a donner un chemin ou tu peux crée une nouvelle potion
+ * filename = nom par défaut du fichier de l'objet
+ * menuName = nom dans le menu quand tu crée
+ *
+ * pour crée un nouveau à partir de sa clique droit dans l'asset --> scriptable objec --> le nom que t'as mis dans menuName
+ */
+[CreateAssetMenu(fileName = "New Potion", menuName = "ScriptableObjects/Consummables/Potion")] 
+public class Potion : Consummable  //hérite de consommable ou équipement dépendant du type 
+{
+
+    //par défaut a un nom, prix, sprite, tu peux ajouter d'autre type en faisant des variables public comme healAmount ici 
+    public int healAmount = 100;
+    public override void Use()
+    {
+        base.Use();
+        /*
+         * met ton code pour appliquer les changement ici, comme c'est un scriptable object tu ne peux pas faire de référence direct dans unity
+         * mais tu peux récupérer des instances tel que :
+         *
+         * private Exemple _exemple
+         *
+         *
+         * private void Start()
+         * {
+         *    _exemple = Exemple.instance;   <-- récupère l'instance du singleton 
+         *     
+         * }
+        */
+    }
+}
---
+++ Unity/Assets/Scripts/Items/Equipment/Helmet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Helmet", menuName = "ScriptableObjects/Equipments/Helmet")] 
+public class Helmet : Equipment
+{
+
+   public int defenseStat = 5; //exemple 
+   public override void Use()
+   {
+      base.Use();
+      //écrit ton code ici 
+   }
+}
