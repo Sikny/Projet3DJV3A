6990fb543ac5c218dd0ab6e8f853897c613b9da5
Quentin <pamartqu@gmail.com>
2020-04-07 13:23:59 +0200

    Build Alpha

--- Unity/Assets/Scripts/Items/ItemSlot.cs
+++ Unity/Assets/Scripts/Items/ItemSlot.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor.IMGUI.Controls;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
