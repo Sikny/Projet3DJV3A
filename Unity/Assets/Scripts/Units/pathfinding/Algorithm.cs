acaef76f4ba3ed9aa7adbbd99cf59d6237257017
brete <bretellealan@hotmail.fr>
2020-04-08 10:06:10 +0200

    Kernel of A Star

---
+++ Unity/Assets/Scripts/Units/pathfinding/Algorithm.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AStar
+{
+    public class Algorithm
+    {
+        protected Graph graph;
+
+        public Algorithm(Graph graph)
+        {
+            this.graph = graph;
+        }
+
+        public void Solve()
+        {
+            graph.Clear();
+            Run();
+        }
+
+        private void Run()
+        {
+            graph.ComputeEstimatedDistance();
+            List<Node> nodeToVisit = graph.NodesList();
+            bool exitReached = false;
+            while (nodeToVisit.Count != 0 && !exitReached)
+            {
+                Node currentNode = nodeToVisit.FirstOrDefault();
+                foreach (var newNode in nodeToVisit)
+                {
+                    if ((newNode.DistanceTraveled + newNode.EstimatedDistance) <
+                        (currentNode.DistanceTraveled + currentNode.EstimatedDistance))
+                    {
+                        currentNode = newNode;
+                    }
+                    
+                }
+
+                if (currentNode == graph.ExitNode())
+                {
+                    exitReached = true;
+                }
+                else
+                {
+                    List<Arc> arcsFromCurrentNode = graph.ArcsList(currentNode);
+                    foreach (var arc in arcsFromCurrentNode)
+                    {
+                        if (arc.From.DistanceTraveled + arc.Cost < arc.To.DistanceTraveled)
+                        {
+                            arc.To.DistanceTraveled = arc.From.DistanceTraveled + arc.Cost;
+                            arc.To.Previous = arc.From;
+                        }
+                    }
+
+                    nodeToVisit.Remove(currentNode);
+                }
+            }
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Units/pathfinding/Arc.cs
@@ -0,0 +1,16 @@
+﻿namespace AStar
+{
+    public class Arc
+    {
+        internal Node From { get; set; }
+        internal Node To { get; set; }
+        internal double Cost { get; set; }
+
+        public Arc(Node from, Node to, double cost)
+        {
+            this.From = from;
+            this.To = to;
+            this.Cost = cost;
+        }
+    }
+}
