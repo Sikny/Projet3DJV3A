0c83b5ed0e276a6fd28428fd4f73d26cfdc982bc
brete <bretellealan@hotmail.fr>
2020-06-07 00:38:36 +0200

    display connection mode

---
+++ Unity/Assets/Scripts/UI/ConnecteMode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ConnecteMode : MonoBehaviour
+{
+    public TMP_Text connecteIndication;
+    // Start is called before the first frame update
+    void Start()
+    {
+         updateConnecteIndication();
+    }
+    public void updateConnecteIndication()
+    {
+        if (connecteIndication != null)
+        {
+            string token = PlayerPrefs.GetString("connection.token");
+            if (token == null || token.Length < 8)
+            {
+                connecteIndication.text = "Non-connecté";
+                connecteIndication.color = Color.white;
+            }
+            else
+            {
+                connecteIndication.text = "Connecté";
+                connecteIndication.color = Color.green;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        updateConnecteIndication();
+    }
+}
