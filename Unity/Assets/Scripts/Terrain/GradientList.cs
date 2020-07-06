58ebe0003072d1f49f6d39ba87138ab3c2ecb44a
Quentin <pamartqu@gmail.com>
2020-07-06 06:56:21 +0200

    Gradient list for arcade mode

---
+++ Unity/Assets/Scripts/Terrain/GradientList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Terrain {
+    [CreateAssetMenu(fileName = "GradientList", menuName = "ScriptableObject/GradientList")]
+    public class GradientList : ScriptableObject {
+        public List<Gradient> gradientsList = new List<Gradient>();
+    }
+}
