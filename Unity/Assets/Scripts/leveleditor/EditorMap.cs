3d97d2fc8a91037d76f0c0f8f617fdbc386fbf3e
Quentin <pamartqu@gmail.com>
2020-07-16 23:46:58 +0200

    mini namespace refactor

--- Unity/Assets/Scripts/leveleditor/EditorMap.cs
+++ Unity/Assets/Scripts/leveleditor/EditorMap.cs
@@ -1,88 +1,87 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using UI;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EditorMap : MonoBehaviour
-{
-    [SerializeField] public ToolsEnum currentTool = ToolsEnum.MAP_BRUSH;
+namespace leveleditor {
+    public class EditorMap : MonoBehaviour
+    {
+        [SerializeField] public ToolsEnum currentTool = ToolsEnum.MAP_BRUSH;
     
-    public GameObject UIToolSelect;
-    public GameObject UIToolRadius;
-    public GameObject UIToolAmplitude;
-    public GameObject UIMinHeight;
-    public GameObject UIMaxHeight;
+        public GameObject UIToolSelect;
+        public GameObject UIToolRadius;
+        public GameObject UIToolAmplitude;
+        public GameObject UIMinHeight;
+        public GameObject UIMaxHeight;
 
-    public GameObject canvasTool;
-    public GameObject canvasInit;
+        public GameObject canvasTool;
+        public GameObject canvasInit;
     
-    public Camera camera;
-    public GameObject mapObject;
+        public Camera camera;
+        public GameObject mapObject;
 
-    public Material[] materialMatchingToToolMode;
-    public enum ToolsEnum
-    {
-        MAP_BRUSH,
-        DIFFICULTY_PAINTING
-    }
-
-    private Map map;
-    private int radiusTool = 20;
-    private int amplitudeTool = 10;
-    private float minHeight = -10;
-    private float maxHeight = 10;
-    
-    public string Size { get; set; }
-    
-    public string Filename { get; set; }
-    
-    public string Money { get; set; }
-
-    public void setRadiusTool()
-    {
-        Slider slider = UIToolRadius.GetComponent<Slider>();
-        radiusTool = (int)slider.value;
-    }
-    
-    public void setAmplitudeTool()
-    {
-        Slider slider = UIToolAmplitude.GetComponent<Slider>();
-        amplitudeTool = (int)slider.value;
-    }
-    
-    public void setMinHeight()
-    {
-        TMP_InputField inputField = UIMinHeight.GetComponent<TMP_InputField>();
-        minHeight = float.Parse(inputField.text);
-    }
-    public void setMaxHeight()
-    {
-        TMP_InputField inputField = UIMaxHeight.GetComponent<TMP_InputField>();
-        maxHeight = float.Parse(inputField.text);
-    }
-    
-    public void setTool()
-    {
-        Dropdown dropdown = UIToolSelect.GetComponent<Dropdown>();
-        switch (dropdown.value)
+        public Material[] materialMatchingToToolMode;
+        public enum ToolsEnum
         {
-            case 0:
-                currentTool = ToolsEnum.MAP_BRUSH;
-                mapObject.GetComponent<MeshRenderer>().material = materialMatchingToToolMode[0];
-                break;
-            case 1:
-                currentTool = ToolsEnum.DIFFICULTY_PAINTING;
-                mapObject.GetComponent<MeshRenderer>().material = materialMatchingToToolMode[1];
-                break;
+            MAP_BRUSH,
+            DIFFICULTY_PAINTING
         }
-    }
 
-    public void generate()
-    {
+        private Map map;
+        private int radiusTool = 20;
+        private int amplitudeTool = 10;
+        private float minHeight = -10;
+        private float maxHeight = 10;
+    
+        public string Size { get; set; }
+    
+        public string Filename { get; set; }
+    
+        public string Money { get; set; }
+
+        public void setRadiusTool()
+        {
+            Slider slider = UIToolRadius.GetComponent<Slider>();
+            radiusTool = (int)slider.value;
+        }
+    
+        public void setAmplitudeTool()
+        {
+            Slider slider = UIToolAmplitude.GetComponent<Slider>();
+            amplitudeTool = (int)slider.value;
+        }
+    
+        public void setMinHeight()
+        {
+            TMP_InputField inputField = UIMinHeight.GetComponent<TMP_InputField>();
+            minHeight = float.Parse(inputField.text);
+        }
+        public void setMaxHeight()
+        {
+            TMP_InputField inputField = UIMaxHeight.GetComponent<TMP_InputField>();
+            maxHeight = float.Parse(inputField.text);
+        }
+    
+        public void setTool()
+        {
+            Dropdown dropdown = UIToolSelect.GetComponent<Dropdown>();
+            switch (dropdown.value)
+            {
+                case 0:
+                    currentTool = ToolsEnum.MAP_BRUSH;
+                    mapObject.GetComponent<MeshRenderer>().material = materialMatchingToToolMode[0];
+                    break;
+                case 1:
+                    currentTool = ToolsEnum.DIFFICULTY_PAINTING;
+                    mapObject.GetComponent<MeshRenderer>().material = materialMatchingToToolMode[1];
+                    break;
+            }
+        }
+
+        public void generate()
+        {
             int sizeDemande = int.Parse(Size);
             int moneyDemande = int.Parse(Money);
             if (10 > sizeDemande || sizeDemande > 100)
@@ -113,35 +112,36 @@ public class EditorMap : MonoBehaviour
             canvasTool.SetActive(true);
             canvasInit.SetActive(false);
        
-    }
-
-    public void save()
-    {
-        if (map != null)
-        {
-            map.save();
-            Popups.instance.Popup("Map was saved!");
-        }
-        else
-        {
-            Popups.instance.Popup("Select settings first!", Color.red);
         }
 
-    }
-
-    private void Start()
-    {
-        Size = "50";
-        Money = "200";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (map != null)
+        public void save()
         {
-            map.updateMapProperties(minHeight, maxHeight);
-            map.UpdateMap(currentTool, radiusTool, amplitudeTool);
+            if (map != null)
+            {
+                map.save();
+                Popups.instance.Popup("Map was saved!");
+            }
+            else
+            {
+                Popups.instance.Popup("Select settings first!", Color.red);
+            }
+
+        }
+
+        private void Start()
+        {
+            Size = "50";
+            Money = "200";
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (map != null)
+            {
+                map.updateMapProperties(minHeight, maxHeight);
+                map.UpdateMap(currentTool, radiusTool, amplitudeTool);
+            }
         }
     }
 }
