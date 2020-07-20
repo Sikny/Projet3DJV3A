02e401aeb262b325824569ee3488b90ec5c8d613
Lior <diler.lior@gmail.com>
2020-07-20 15:06:59 +0200

    implemented potions and added to story mode and arcade mode

--- Unity/Assets/Scripts/Items/Consumables/MagicHealthPotion.cs
+++ Unity/Assets/Scripts/Items/Consumables/MagicHealthPotion.cs
@@ -11,8 +11,8 @@ using UnityEngine;
  * pour crée un nouveau à partir de sa clique droit dans l'asset --> scriptable objec --> le nom que t'as mis dans menuName
  */
 namespace Items.Consumables {
-    [CreateAssetMenu(fileName = "HealPotion", menuName = "ScriptableObject/Consumables/HealPotion")] 
-    public class HealPotion : Consumable  //hérite de consommable ou équipement dépendant du type 
+    [CreateAssetMenu(fileName = "MagicHealPotion", menuName = "ScriptableObject/Consumables/MagicHealthPotion")] 
+    public class MagicHealthPotion : Consumable  //hérite de consommable ou équipement dépendant du type 
     {
 
         //par défaut a un nom, prix, sprite, tu peux ajouter d'autre type en faisant des variables public comme healAmount ici 
@@ -23,7 +23,7 @@ namespace Items.Consumables {
                 for (int i = 0; i < UnitLibData.selectedUnit.entityCount; i++)
                 {
                     if (UnitLibData.selectedUnit.GetEntity(i) != null && UnitLibData.selectedUnit.GetEntity(i).GetLife() > 0) // REGEN IF LIFE > 0
-                        UnitLibData.selectedUnit.GetEntity(i).SetLife(UnitLibData.selectedUnit.GetEntity(i).GetLife() + (UnitLibData.selectedUnit.GetEntity(i).GetMaxLife()/2));
+                        UnitLibData.selectedUnit.GetEntity(i).ResetLife();
                 }
                 base.Use();
             }
